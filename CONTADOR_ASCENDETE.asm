fdb56bdf96044997a288c8a4b030e7baf7775115
Rodrigo <116986161+Rotronica@users.noreply.github.com>
2025-03-18 22:49:23 -0400

    CONTADOR_ASCENDETE.asm

--- CONTADOR_ASCENDETE.asm
+++ CONTADOR_ASCENDETE.asm
@@ -11,7 +11,7 @@
 ;se utiliza la técnica de multiplexación.
 
 ;El archivo de simulación es CONTADOR_ASCENDENTE_INTRB0.sim1 se realiza con el software simulide.exe
-,que se encuentra en la carpeta SimulIDE_1.1.0-SR0_Win64
+;que se encuentra en la carpeta SimulIDE_1.1.0-SR0_Win64
 
 		__CONFIG _XT_OSC & _WDTE_OFF & _PWRTE_ON & _CP_OFF & _LVP_OFF & _BOREN_OFF
 		LIST	P=16F877A
